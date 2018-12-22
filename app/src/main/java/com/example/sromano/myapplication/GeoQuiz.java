921bf3a8a405fc09b7be65c59eb516bc1afa3410
sromano <sam.romano18@gmail.com>
2018-12-21 18:08:53 -0800

    Added more questions and configured buttons.

--- app/src/main/java/com/example/sromano/myapplication/GeoQuiz.java
+++ app/src/main/java/com/example/sromano/myapplication/GeoQuiz.java
@@ -11,19 +11,33 @@ public class GeoQuiz extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private int mCurrentIndex = 0;
 
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_asia, true)
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_quiz);
 
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        updateQuestion();
+
+
         mTrueButton = (Button) findViewById(R.id.true_btn);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(GeoQuiz.this, R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+               checkAnswer(true);
             }
         });
 
@@ -32,12 +46,36 @@ public class GeoQuiz extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(GeoQuiz.this, R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
-
+                checkAnswer(false);
             }
         });
 
+        mNextButton = (Button) findViewById(R.id.next_btn);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+             updateQuestion();
+            }
+        });
+
+    }
+
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    private void checkAnswer(boolean userPressed) {
+        boolean answer = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageID = 0;
+
+        if(answer == userPressed) {
+            messageID = R.string.correct_toast;
+        }
+        else { messageID = R.string.incorrect_toast; }
+
+        Toast.makeText(GeoQuiz.this, messageID, Toast.LENGTH_SHORT).show();
     }
 
 }
