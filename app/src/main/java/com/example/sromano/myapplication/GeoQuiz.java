de8742b4e7f49cc4322cbc67feea29cd932bf247
sromano <sam.romano18@gmail.com>
2018-12-21 17:18:53 -0800

    Added First Quiz Question with Toast messages.

--- app/src/main/java/com/example/sromano/myapplication/GeoQuiz.java
+++ app/src/main/java/com/example/sromano/myapplication/GeoQuiz.java
@@ -2,28 +2,42 @@ package com.example.sromano.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GeoQuiz extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    private Button mTrueButton;
+    private Button mFalseButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_quiz);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        mTrueButton = (Button) findViewById(R.id.true_btn);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoQuiz.this, R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        mFalseButton = (Button) findViewById(R.id.false_btn);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GeoQuiz.this, R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
 }
